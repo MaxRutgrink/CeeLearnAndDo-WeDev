da674b4d1ad416be8bd4a0da347ba4b6d5488b0b
Jelle Venema <jelle@venerik.com>
2019-10-03 14:25:53 +0200

    Added Question Class in Models folder

---
+++ CeeLearnAndDo WeDev/Models/Question.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CeeLearnAndDo_WeDev.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public int CategoryId { get; set; }
+        public string Content { get; set; }
+        public DateTime CreationDate { get; set; }
+        public bool IsVisible { get; set; }
+        public string Title { get; set; }
+    }
+}
