f41fd00f0dd64daff515648636e327dae4a9e566
Jelle Venema <jelle@venerik.com>
2019-10-04 10:04:05 +0200

    Added Classes ArticleComment, Category, ConsultantExpertise and Expertise in folder Models

---
+++ CeeLearnAndDo WeDev/Models/ArticleComment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CeeLearnAndDo_WeDev.Models
+{
+    public class ArticleComment
+    {
+        public int Id { get; set; }
+        public int ArticleId { get; set; }
+        public int UserId { get; set; }
+        public string Content { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
---
+++ CeeLearnAndDo WeDev/Models/ConsultantExpertise.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CeeLearnAndDo_WeDev.Models
+{
+    public class ConsultantExpertise
+    {
+        public int ConsultantId { get; set; }
+        public int ExpertiseId { get; set; }
+    }
+}
