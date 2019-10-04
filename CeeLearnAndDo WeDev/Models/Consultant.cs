078e00de6b6228e193752e01c7be56de93234b13
Jelle Venema <jelle@venerik.com>
2019-10-04 09:31:30 +0200

    Added Consultant Class in Models folder

---
+++ CeeLearnAndDo WeDev/Models/Consultant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CeeLearnAndDo_WeDev.Models
+{
+    public class Consultant
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string ImageUrl { get; set; }
+        public string Biography { get; set; }
+    }
+}
