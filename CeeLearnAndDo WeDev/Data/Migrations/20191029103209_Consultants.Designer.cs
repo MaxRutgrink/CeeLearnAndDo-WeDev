e1fcd424b98f77c815bb177edc53c798efba2110
Mees Klaver <mees.klaver@outlook.com>
2019-10-29 13:42:09 +0100

    QuestionComment model FK toegevoegd

--- CeeLearnAndDo WeDev/Data/Migrations/20191029103209_Consultants.Designer.cs
+++ CeeLearnAndDo WeDev/Data/Migrations/20191029103209_Consultants.Designer.cs
@@ -4,14 +4,16 @@ using CeeLearnAndDo_WeDev.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CeeLearnAndDo_WeDev.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191029103209_Consultants")]
+    partial class Consultants
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
